83f884e538a747d48b2cc193428093164bf501a0
mniszka <aflisnik@wp.pl>
2021-04-12 12:33:29 +0200

    Update and rename increment, decrement to increment, decrement.cpp

---
+++ increment, decrement.cpp
@@ -0,0 +1,17 @@
+/* 
+pre-increment ++a
+increment the value of a variable before using it in a expression
+
+post-increment a++
+increment the value of variable after executing expression completely in which post increment is used
+value is first used in a expression and then incremented. 
+
+pre-decrement --a
+post-decrement a--
+
+*/
+
+
+  
+  
+
