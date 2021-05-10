8d2a406f091b74bf41400e0ffc592181991ff678
mniszka <aflisnik@wp.pl>
2021-05-10 12:17:12 +0200

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -5,8 +5,14 @@ using namespace std;
 
 int main(){
 
-int tab[3]= {1,2,3};
-cout<<*tab; // = tab[0] = 1
-cout<<*(tab+2); //=tab[2] = 3
+int arr[3]= {1,2,3};
+cout<<*arr; // = tab[0] = 1
+cout<<*(arr+2); //=tab[2] = 3
+  
+ //size in bytes
+ cout<< sizeof(arr);
+   
+ //number of elements
+  cout<<sizeof(arr)/sizeof(arr[0]);
 
 }
