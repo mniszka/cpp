7b5d61f7bd3d048dbaab0e8dc3baff4cb4c721eb
mniszka <aflisnik@wp.pl>
2021-05-25 18:38:39 +0200

    Create declaration and definition.c

---
+++ declaration and definition.c
@@ -0,0 +1,8 @@
+int age; // declaration of a variable
+age = 3; // definition of a variable
+
+int fun(int a); //declaration of a function
+
+main cat() //main function, the program starts here
+{
+}
