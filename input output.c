00c0e4a731bb2fc0c959d08671cb582a50a89b4f
mniszka <aflisnik@wp.pl>
2021-05-26 17:47:09 +0200

    Update and rename iostream.c to input output.c

--- input output.c
+++ input output.c
@@ -1,4 +1,5 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
@@ -9,14 +10,24 @@ printf("My name is %s and I'm %d years old\n", characterName, age); //printf is 
 
 age = 4;
 printf("I'm %d years old", age);
-
-return 0;
-}
-/*
+  /*
 %s -> string of letters
 %d -> decimal integer
 %i -> integer
 %f -> floating point number
+%lf -> double 
+%c -> character
+
 
 %d assume base 10 while %i auto detects the base
 */
+  
+int age; //declare a variable
+printf("Enter yoaur age\n");
+scanf("%d", &age);//pointer
+printf("You are %d years old", age);
+    
+
+return 0;
+}
+
