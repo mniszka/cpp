9120321d210dbe5cfdfe3ece9cc281e04f6274d6
mniszka <aflisnik@wp.pl>
2021-05-26 17:48:03 +0200

    Update data types.c

--- data types.c
+++ data types.c
@@ -1,5 +1,9 @@
 int age = 30; //it stores an integer (whole number)
 double gpa = 3.7; // decimal number
-float ;//decimal number
+float number = 3.71;//decimal number
 char grade = 'A';// single character
 char name[] = "Anna German";// string of characters which cannot be modified
+
+
+
+
