ada1b601191ab2959945d89aaa55da950877cbeb
mniszka <aflisnik@wp.pl>
2021-05-26 18:24:06 +0200

    Create char.c

---
+++ char.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+ char cat_name[20]; //allocates the memory for the variable
+ printf ("Enter your cat's name\n");
+ scanf ("%s", cat_name);
+ printf ("Your cat's name is %s", cat_name);
+  
+  return 0;
+}
