8bddff428e672fe80e441ae124c03123228f1bd9
mniszka <aflisnik@wp.pl>
2021-05-27 11:03:37 +0200

    Update input output.c

--- input output.c
+++ input output.c
@@ -1,13 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
 char characterName[] = "Mikan";// store a bunch of characters
 int age = 3;
-
 printf("My name is %s and I'm %d years old\n", characterName, age); //printf is a function
-
 age = 4;
 printf("I'm %d years old", age);
   /*
@@ -18,16 +12,26 @@ printf("I'm %d years old", age);
 %lf -> double 
 %c -> character
 
-
 %d assume base 10 while %i auto detects the base
 */
   
+//printf:
 int age; //declare a variable
 printf("Enter yoaur age\n");
 scanf("%d", &age);//pointer
 printf("You are %d years old", age);
+  
+//fgets:
+char name[20];
+printf ("Enter your name and surname \n");
+fgets(name, 20, stdin);
+printf ("Your name is %s", name);
+
+//be aware:
+printf ("Your name is %s xxxxxxx", name); //xxxxxxx will be displayed in a new line
+  
+  
     
 
-return 0;
-}
+
 
