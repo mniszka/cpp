b900e691565f1e82ec3b5cea01fd5d0ae7124faf
mniszka <aflisnik@wp.pl>
2021-05-30 21:34:11 +0200

    Create 2D arrays.c

---
+++ 2D arrays.c
@@ -0,0 +1,9 @@
+//2-dimensional arrays- alle elements in the array are arrays themeselves
+
+ int arr[3][2]= { 
+                {1,2}, 
+                {3,4}, 
+                {5,6}
+                };
+ printf("%d", arr[0][0]);
+
