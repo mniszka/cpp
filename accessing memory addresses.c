bb3dbab7ab901ed57ba0f0664a2c401b4a10b650
mniszka <aflisnik@wp.pl>
2021-05-31 10:47:44 +0200

    Create accessing memory addresses.c

---
+++ accessing memory addresses.c
@@ -0,0 +1,23 @@
+/*while running a program a computer stores information in RAM memory.
+Values like int/char are stored in a specific location.
+Whan I want to refer to the value I use a name but a program uses memory address when this value is stored inside a physical memory
+*/
+
+//How to print out a physical memory address:
+
+
+ int number = 3;
+ printf("%p", &number);//%p -> pointer
+
+ //the result will be in hexadecimal number
+
+
+
+
+
+
+
+
+
+
+
