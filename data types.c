9eb63c839005f88fb9d29d08944e56d3a22429bb
mniszka <aflisnik@wp.pl>
2021-05-31 10:55:14 +0200

    Update data types.c

--- data types.c
+++ data types.c
@@ -3,6 +3,7 @@ double gpa = 3.7; // decimal number
 float number = 3.71;//decimal number
 char grade = 'A';// single character
 char name[] = "Anna German";// string of characters which cannot be modified
+$age //pointer- it stores a memory adress of the "age" variable
 
 
 
