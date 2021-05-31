32ca96c48b3e079c7668814294436740d0b212cc
mniszka <aflisnik@wp.pl>
2021-05-31 10:34:44 +0200

    Update 2D arrays.c

--- 2D arrays.c
+++ 2D arrays.c
@@ -1,11 +1,32 @@
 //2-dimensional arrays- alle elements in the array are arrays themeselves
 
- int arr[3][2]= { 
-                {1,2}, 
-                {3,4}, 
-                {5,6}
-                };
- printf("%d", arr[0][0]);
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ int arr[3][2]= { {1,2}, {3,4}, {5,6}};
+ //printf("%d", arr[0][0]);
+
+int i, j;
+ for(i=0; i<3; i++){
+   for(j=0; j<2; j++){
+     printf("%d", arr[i][j]);//123456
+   }
+ }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
