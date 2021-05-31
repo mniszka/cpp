2a7e6eca13921cab57b6d8710d918c254d7a0811
mniszka <aflisnik@wp.pl>
2021-05-31 15:00:02 +0200

    Create pointers.c

---
+++ pointers.c
@@ -0,0 +1,11 @@
+// pointer is a type of data, it is a memory address
+
+// * astrix
+// & ampersand
+
+int number = 3;
+
+///gives pointer variable a name:
+int * pNumber =  &number;// put "p" before a name
+
+printf("%p", pNumber );
